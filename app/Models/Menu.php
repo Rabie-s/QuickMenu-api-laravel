579383f76b9e59e3912875b4dd9cb338833c99f6
Rabie-s <asdfw.jsd@gmail.com>
2026-01-17 15:31:20 +0300

    add HasUuids

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 
 class Menu extends Model
@@ -18,7 +18,7 @@ class Menu extends Model
         'is_available',
     ];
 
-    
+
     public function uniqueIds(): array
     {
         return ['uuid'];
